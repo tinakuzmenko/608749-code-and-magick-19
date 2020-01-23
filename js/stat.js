31308427c672615be11dfd8fe38cb5127fd29480
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-01-23 17:48:30 +0100

    Заканчивает домашнее задание по учебному проекту

--- js/stat.js
+++ js/stat.js
@@ -1,37 +1,70 @@
 'use strict';
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 280;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
-var CLOUD_Y = 0;
-var SHADOW_GAP = 10;
-var CONTENT_GAP = 30;
+var CLOUD_Y = 10;
+var GAP = 10;
+var CONTENT_GAP = 20;
 var CONTENT_X = CLOUD_X + CONTENT_GAP;
 var CONTENT_Y = CLOUD_Y + CONTENT_GAP;
+var TEXT_WIDTH = 50;
+var BAR_WIDTH = 40;
+var barHeight = 150;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+var createFontStyle = function (ctx, font, baseline, style) {
+  ctx.font = font;
+  ctx.textBaseline = baseline;
+  ctx.fillStyle = style;
+};
 
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillStyle = '#000';
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+var paintBar = function (ctx, currentPlayer) {
+  var saturation = (Math.random() * 100) + '%';
+  var randomBlue = 'hsl(240, ' + saturation + ', 50%)';
+
+  if (currentPlayer === 'Вы') {
+    ctx.fillStyle = 'hsl(0, 100%, 50%)';
+  } else {
+    ctx.fillStyle = randomBlue;
+  }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  createFontStyle(ctx, '16px PT Mono', 'hanging', '#000');
   ctx.fillText('Ура вы победили!', CONTENT_X, CONTENT_Y);
   ctx.fillText('Список результатов:', CONTENT_X, CONTENT_Y + CONTENT_GAP);
 
-  ctx.fillText(names[0], CONTENT_X, 105);
-  ctx.fillRect(180, 105, 380, 20);
+  var maxTime = getMaxElement(times);
 
-  ctx.fillText(names[1], CONTENT_X, 145);
-  ctx.fillRect(180, 145, 380, 20);
+  for (var i = 0; i < names.length; i++) {
+    var currentX = CONTENT_X + (BAR_WIDTH + TEXT_WIDTH) * i;
+    var currenBarHeight = (barHeight * times[i]) / maxTime;
+    var currentY = CLOUD_HEIGHT - CONTENT_GAP - GAP - currenBarHeight;
+    var currentTime = Math.floor(times[i]);
 
-  ctx.fillText(names[2], CONTENT_X, 185);
-  ctx.fillRect(180, 185, 380, 20);
+    createFontStyle(ctx, '16px PT Mono', 'hanging', '#000');
+    ctx.fillText(names[i], currentX, CLOUD_HEIGHT - CONTENT_GAP);
+    ctx.fillText(currentTime, currentX, currentY - CONTENT_GAP);
 
-  ctx.fillText(names[3], CONTENT_X, 225);
-  ctx.fillRect(180, 225, 380, 20);
+    paintBar(ctx, names[i]);
+    ctx.fillRect(currentX, currentY, BAR_WIDTH, currenBarHeight);
+  }
 };
